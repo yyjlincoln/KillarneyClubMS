f67a3a57616b216649ebdfa751a4ac6015e65c07
yyjlincoln <yyjlincoln@gmail.com>
2020-06-16 12:05:44 +1000

    Functionality Updates

--- KillarneyClubManagingSystem/KillarneyClubManagingSystem/Scoreboard.Designer.vb
+++ KillarneyClubManagingSystem/KillarneyClubManagingSystem/Scoreboard.Designer.vb
@@ -26,9 +26,10 @@ Partial Class Scoreboard
         '
         'Scoreboard
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 24.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(537, 339)
+        Me.ClientSize = New System.Drawing.Size(1126, 712)
+        Me.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.Name = "Scoreboard"
         Me.Text = "Scoreboard"
         Me.ResumeLayout(False)
